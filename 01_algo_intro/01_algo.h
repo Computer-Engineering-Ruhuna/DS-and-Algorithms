61094c053ea164366fdaa051b1a4c25a37d32f62
rajitha-ruh <rajitha@eie.ruh.ac.lk>
2022-10-25 16:14:41 +0530

    array create 3

--- 01_algo_intro/01_algo.h
+++ 01_algo_intro/01_algo.h
@@ -2,14 +2,16 @@
 #include<cstdlib>
 using namespace std;
 
-int main(){
-    int size = 10;
+int* create_int_array(int size){
     int* A = new int[size];
 
     for(int i=0;i<size;i++){
         A[i] = rand()%1000;
     }
+    return A;
+}
 
+void print_array(int*A, int size){
     for(int i=0;i<size;i++){
         cout<<A[i]<<" ";
     }
---
+++ 01_algo_intro/01_rand_array_2.cc
@@ -0,0 +1,8 @@
+#include "01_algo.h"
+
+int main(){
+    int size = 10;
+    int* A = create_int_array(size);
+    print_array(A,size);
+    return 0;    
+}
